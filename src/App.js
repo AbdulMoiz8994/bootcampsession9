d8297bb25ab700fa2bf70841f178fb9fa01d5434
AbdulMoiz8994 <moiza8994@gmail.com>
2021-02-14 11:07:21 +0500

    we used material Ui for desgin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import {SvgIcon,TextField ,Checkbox,FormControlLabel,Button,Link } from '@material-ui/core';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: '40 auto',
+      textAlign: 'center',
+    marginTop: "50px"
+  },
+  home:{
+    fontSize: '40px'
+  },
+  form:{
+    textAlign: 'center',
+    width: "100%"
+  },
+  checkbox:{
+  marginTop: '20px',
+  marginLeft: '-90px'
+  },
+  button:{
+    width: "15%",
+  },
+  Link:{
+  margin: '10px',
+     
+  }
+}));
+
+function HomeIcon(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
   );
 }
 
-export default App;
+export default function SvgIconsColor() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <HomeIcon color="secondary" className={classes.home} />
+      <h2 style={{textAlign: "center"}}>Sign in</h2>
+      <div  className={classes.form}>
+      <form noValidate autoComplete="off">
+      <TextField id="outlined-basic" label="Email" variant="outlined" /><br/><br/><br/>
+      <TextField id="outlined-basic" label="Password" variant="outlined" />
+    </form>
+      </div>
+      <FormControlLabel className={classes.checkbox}
+        control={
+          <Checkbox
+          name="checkedB"
+          color="primary"
+          
+          />
+        }
+        label="Remember me"
+      />
+      <br/><br/>
+
+      <Button variant="contained" className={classes.button} color="primary" disableElevation>
+      Sign up
+    </Button>
+    <br/><br/>
+    
+    <Link
+      component="button"
+      variant="body2"
+      // onClick={() => {
+      //   console.info("I'm a button.");
+      // }}
+    >
+      Forget Password
+    </Link>
+    <br/>
+    <Link 
+    className={classes.Link}
+      component="button"
+      variant="body2"
+      onClick={() => {
+        console.info("I'm a button.");
+      }}
+    >
+      Don't have an account ? Sign Up
+    </Link>
+    </div>
+
+  );
+}
